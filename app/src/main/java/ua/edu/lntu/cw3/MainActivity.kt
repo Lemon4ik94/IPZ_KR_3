528fb05851af7dc8de7f330446a35217d2d57310
Lemon4ik94 <olegborbych@gmail.com>
2024-02-24 12:27:48 +0200

    Create base functions

--- app/src/main/java/ua/edu/lntu/cw3/MainActivity.kt
+++ app/src/main/java/ua/edu/lntu/cw3/MainActivity.kt
@@ -3,12 +3,19 @@ package ua.edu.lntu.cw3
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
+import androidx.compose.foundation.horizontalScroll
+import androidx.compose.foundation.layout.Box
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.FlowColumnScopeInstance.align
+import androidx.compose.foundation.layout.FlowRowScopeInstance.align
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Surface
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.modifier.modifierLocalConsumer
 import androidx.compose.ui.tooling.preview.Preview
 import ua.edu.lntu.cw3.ui.theme.IPZ_KR_3Theme
 
@@ -22,7 +29,7 @@ class MainActivity : ComponentActivity() {
                     modifier = Modifier.fillMaxSize(),
                     color = MaterialTheme.colorScheme.background
                 ) {
-                    Greeting("Android")
+                    MyContent("Android")
                 }
             }
         }
@@ -30,17 +37,38 @@ class MainActivity : ComponentActivity() {
 }
 
 @Composable
-fun Greeting(name: String, modifier: Modifier = Modifier) {
-    Text(
-        text = "Hello $name!",
-        modifier = modifier
-    )
+fun MyContent() {
+    Box(contentAlignment = Alignment.Center) {
+        VerticalList()
+    }
+
+
 }
 
-@Preview(showBackground = true)
 @Composable
-fun GreetingPreview() {
+fun VerticalList() {
+    Column() {
+
+    }
+}
+
+@Composable
+fun Rows() {
+
+}
+
+@Composable
+fun ListElement(title: String, desc: String) {
+    Column {
+        Text(text = "title")
+        Text(text = "desc")
+    }
+}
+
+@Preview(showBackground = true, showSystemUi = true)
+@Composable
+fun ContentPreview() {
     IPZ_KR_3Theme {
-        Greeting("Android")
+        MyContent()
     }
 }
