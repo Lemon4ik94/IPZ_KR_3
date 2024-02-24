b7293deff08127354d41c7ab39a91dd46720b85e
Lemon4ik94 <olegborbych@gmail.com>
2024-02-24 13:47:10 +0200

    Something working again

--- app/src/main/java/ua/edu/lntu/cw3/MainActivity.kt
+++ app/src/main/java/ua/edu/lntu/cw3/MainActivity.kt
@@ -1,12 +1,19 @@
 package ua.edu.lntu.cw3
 
+import android.health.connect.datatypes.units.Length
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.Row
+import androidx.compose.foundation.layout.Spacer
 import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.height
+import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.lazy.grid.GridCells
+import androidx.compose.foundation.lazy.grid.LazyVerticalGrid
+import androidx.compose.foundation.lazy.grid.items
 import androidx.compose.material3.Card
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Surface
@@ -15,6 +22,8 @@ import androidx.compose.runtime.Composable
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
+import ua.edu.lntu.cw3.data.Datasource
 import ua.edu.lntu.cw3.model.Information
 import ua.edu.lntu.cw3.ui.theme.IPZ_KR_3Theme
 
@@ -47,16 +56,30 @@ fun Content(information: Information, modifier: Modifier) {
     Card(modifier = modifier) {
         Column {
             Text(text = information.name)
+            Spacer(modifier.height(5.dp))
             Text(text = information.desc)
         }
     }
 }
 
-
-@Preview(showBackground = true)
 @Composable
-fun infoPreview() {
-    Content(Information("Oleh", "Borbych"), modifier = Modifier)
+fun InfoList(informationList: List<Information>, modifier: Modifier) {
+    LazyVerticalGrid(columns = GridCells.Fixed(2)) {
+        items(informationList) {
+            information -> Content(
+                information = information,
+                modifier = Modifier)
+        }
+    }
+}
+
+
+@Preview(showBackground = true, showSystemUi = true)
+@Composable
+fun InfoPreview() {
+    IPZ_KR_3Theme {
+        InfoList(informationList = Datasource().loadInformation(), modifier = Modifier)
+    }
 }
 
 
--- app/src/main/java/ua/edu/lntu/cw3/model/Information.kt
+++ app/src/main/java/ua/edu/lntu/cw3/model/Information.kt
@@ -1,5 +1,7 @@
 package ua.edu.lntu.cw3.model
 
+import androidx.annotation.StringRes
+
 data class Information(
     val name: String,
     val desc: String
