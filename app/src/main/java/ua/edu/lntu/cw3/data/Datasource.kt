fdc03e049b27df0dbbd60c176aa92d1bad65ad23
Lemon4ik94 <olegborbych@gmail.com>
2024-02-24 12:56:10 +0200

    Created dataclasses

---
+++ app/src/main/java/ua/edu/lntu/cw3/data/Datasource.kt
@@ -0,0 +1,16 @@
+package ua.edu.lntu.cw3.data
+import ua.edu.lntu.cw3.model.Information
+
+data class Datasource() {
+    fun loadInformation(): List<Information> {
+        Information("Oleh", "Borbych"),
+        Information("Alex", "Xmils"),
+        Information("Bohdan", "Vyrymchuk"),
+        Information("Anton", "Karpyuk"),
+        Information("Nazar", "Nyshchiy"),
+        Information("Oleh", "Borbych"),
+        Information("Oleh", "Borbych"),
+        Information("Oleh", "Borbych"),
+
+    }
+}
---
+++ app/src/main/java/ua/edu/lntu/cw3/model/Information.kt
@@ -0,0 +1,6 @@
+package ua.edu.lntu.cw3.model
+
+data class Information(
+    val name: String,
+    val desc: String
+)
