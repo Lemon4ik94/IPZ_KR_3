320dbc65fd1d3811681acaceb842b0b44ae26299
Lemon4ik94 <olegborbych@gmail.com>
2024-02-24 12:58:31 +0200

    Edit errors

--- app/src/main/java/ua/edu/lntu/cw3/MainActivity.kt
+++ app/src/main/java/ua/edu/lntu/cw3/MainActivity.kt
@@ -3,20 +3,19 @@ package ua.edu.lntu.cw3
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
-import androidx.compose.foundation.horizontalScroll
 import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.Column
-import androidx.compose.foundation.layout.FlowColumnScopeInstance.align
-import androidx.compose.foundation.layout.FlowRowScopeInstance.align
+import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.material3.Card
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Surface
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
-import androidx.compose.ui.modifier.modifierLocalConsumer
 import androidx.compose.ui.tooling.preview.Preview
+import ua.edu.lntu.cw3.model.Information
 import ua.edu.lntu.cw3.ui.theme.IPZ_KR_3Theme
 
 class MainActivity : ComponentActivity() {
@@ -29,7 +28,7 @@ class MainActivity : ComponentActivity() {
                     modifier = Modifier.fillMaxSize(),
                     color = MaterialTheme.colorScheme.background
                 ) {
-                    MyContent("Android")
+                    MyContent()
                 }
             }
         }
@@ -39,29 +38,17 @@ class MainActivity : ComponentActivity() {
 @Composable
 fun MyContent() {
     Box(contentAlignment = Alignment.Center) {
-        VerticalList()
-    }
-
-
-}
-
-@Composable
-fun VerticalList() {
-    Column() {
 
     }
 }
 
 @Composable
-fun Rows() {
-
-}
-
-@Composable
-fun ListElement(title: String, desc: String) {
-    Column {
-        Text(text = "title")
-        Text(text = "desc")
+fun Content(information: Information, modifier: Modifier) {
+    Card(modifier = modifier) {
+        Column {
+            Text(text = information.name)
+            Text(text = information.desc)
+        }
     }
 }
 
